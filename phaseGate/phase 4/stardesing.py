980f903e5f977d209155a31718f924cf6a957efe
Okechukwu Peter <okechukwupeter10@gmail.com>
2024-10-10 18:01:55 +0100

    phase 4

--- phaseGate/phase 4/stardesing.py
+++ phaseGate/phase 4/stardesing.py
@@ -1,13 +1,26 @@
 
-number = 0
-for numbers in range(7):
-	if numbers % 2 ==0:
-		print("*" * numbers)
-	else:
-		value = str(numbers)
-		print(value * numbers)
+
+for count in range(1,7):
+	print()
+	for counter in range(1,count):
+		if counter % 2 == 0:
+			print("*" , end = ' ')
+		if counter % 2 !=0:
+			print(counter , end = ' ')
+
+
+for count in range(7,1,-1):
+	print()
+	for counter in range(1,count):
+		if counter % 2 == 0:
+			print("*" , end = ' ')
+		if counter % 2 !=0:
+			print(counter , end = ' ')
 
 	
+
+
+
 
 
 
