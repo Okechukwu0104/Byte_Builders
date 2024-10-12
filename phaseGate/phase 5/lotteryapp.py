faa784f07b1682764b5d084d8ea4bfaa8f85cf19
Okechukwu Peter <okechukwupeter10@gmail.com>
2024-10-12 01:24:59 +0100

    phase 5 changes

--- phaseGate/phase 5/lotteryapp.py
+++ phaseGate/phase 5/lotteryapp.py
@@ -1,43 +1,46 @@
-user_input = input("input your 3 numbers")
-count = len(user_input)
-in_built = [3,3,0]
-comma = ascii(",")
-space = ascii(" ")
+import random
 
+user_input = input("input your 3 numbers: ")
+count = len(user_input)
+count = 0
+in_built = []
+while count !=3:
+	value = random.randrange(1,10)
+	in_built.insert(count, value)
+	count +=1
+print(in_built)
 user = []
 
 if count == 3:
 	user_input = int(user_input)
 	for digits in range(count):
 		extract = abs(user_input % 10)			
-		user.insert(count, extract)
+		user.insert(0, extract)
 		remain = user_input // 10
 		user_input = remain
 		
 		
-	counter = 3
 	exact_count = 0
 	residual_count = 0
-	for numbers in range(counter):
-		if user[numbers] == in_built[numbers]:
-			exact_count+=1
-		else:
-			for count in range(counter):
-				digit = user[count]
-				for count in range(counter):
-					if digit == in_built[count]:
-						residual_count+=1
+
+	for count in range(3):
+		if user[count] == in_built[count]:
+			exact_count += 1
+		elif user[count] in in_built:
+			residual_count += 1
+
 			
 	
-	if exact_count == 3:
+	if exact_count == 3 :
 		print("congrats..you have won $5000! (Highest prize)")
 	
 	elif exact_count == 2 or residual_count ==2 :
 		print("congrats..you have won $4000! (2nd runner up)")
-	else:
+	elif exact_count == 1 or residual_count ==1 :
 		print("congrats..you have won $1000! (3rd runner up)")
 
-		
+	else:
+		print("you didnt win any prize")
 	
 	
 
