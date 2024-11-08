c3338960160d2c95dc3ef1b9b6a6bd9332c62d9a
Okechukwu Peter <okechukwupeter10@gmail.com>
2024-11-08 21:39:12 +0100

    commited

--- Class_work/Firedrill/firepractice.py
+++ Class_work/Firedrill/firepractice.py
@@ -1,29 +1,62 @@
+import math
 import random
+
+
 def list_of_integers():
     num_array = []
-    for i in range(0, 6):
+    for i in range(0, 17):
         random_digit = random.randrange(1, 16)
         num_array.append(random_digit)
     return num_array
-list_of_integers()
 
-the_array = [4,3,2,1]
+
+the_array = list_of_integers()
 length = len(the_array)
+numbers = the_array * 2
 
 
-def duplicate_elements(the_array,length):
+def duplicate_elements(the_array, length):
     new_array = []
     for count in range(0, length):
         value = the_array[count]
         new_array.append(value)
     final_array = the_array + new_array
     return final_array
-duplicate_elements(the_array,length)
 
-numbers = [4, 3, 2, 1, 4, 3, 2, 1]
-def duplicate_elimination(numbers,length):
-    for count in numbers:
-        if numbers[count] == numbers[count+(length+1)]:
-            numbers.remove(numbers[count])
+
+duplicate_elements(the_array, length)
+
+
+def duplicate_elimination(numbers, length):
+    for count in range(0, length):
+        numbers.pop()
     return numbers
-duplicate_elimination(numbers,length)
+
+
+duplicate_elimination(numbers, length)
+
+
+def sum_of_3rd_index(numbers):
+    total = 0
+    length = len(numbers)
+    for digits in range(2, length, 3):
+        total += int(numbers[digits])
+    return total
+
+
+sum_of_3rd_index(numbers)
+
+def sum_of_3_indexes(numbers):
+    ave = int(len(numbers) / 2)
+    if len(numbers) % 2 == 0:
+        ave = ave-1
+        middle = int(numbers[ave] + numbers[ave+1])/2
+    else:
+        middle = numbers[math.ceil(ave)]
+    print(middle)
+
+    total = middle + numbers[0] + numbers[len(numbers)-1 ]
+    return total
+
+
+sum_of_3_indexes(numbers)
--- Class_work/Firedrill/test_firepractice.py
+++ Class_work/Firedrill/test_firepractice.py
@@ -11,14 +11,32 @@ class Testfirepractice(unittest.TestCase):
         self.assertEqual(random, random)
 
     def test_to_check_if_elements_are_duplicated(self):
-        the_array = [4, 3, 2, 1]
-        expected = [4, 3, 2, 1, 4, 3, 2, 1]
-        result = firepractice.duplicate_elements(the_array)
+        the_array = firepractice.list_of_integers()
+        expected = the_array * 2
+        length = len(the_array)
+        result = firepractice.duplicate_elements(the_array, length)
         self.assertEqual(expected, result)
 
-    def test_if_duplicate_elimination_succeds(self):
-        numbers = [4, 3, 2, 1, 4, 3, 2, 1]
-        expected = [4, 3, 2, 1]
-        length = 8
+    def test_if_duplicate_elimination_works(self):
+        expected = firepractice.list_of_integers()
+        numbers = expected * 2
+        length = len(expected)
         result = firepractice.duplicate_elimination(numbers, length)
+        self.assertEqual(expected, result)
+
+    def test_that_every_3rd_element_is_added(self):
+        numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+        result = 9
+        expected = firepractice.sum_of_3rd_index(numbers)
+        self.assertEqual(expected, result)
+
+    def test_to_sum_first_middle_last_elements(self):
+        numbers = [1, 2, 2, 6]
+        result = firepractice.sum_of_3_indexes(numbers)
+        expected = 9.0
+        self.assertEqual(expected, result)
+
+        numbers = [1, 2, 4, 5, 6]
+        result = firepractice.sum_of_3_indexes(numbers)
+        expected = 11
         self.assertEqual(expected, result)
